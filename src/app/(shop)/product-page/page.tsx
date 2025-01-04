1aefa6947c06b4114d66c8862bbbd05b08e9571e
karloCroDev <karlo.webdev@gmail.com>
2025-01-04 22:41:39 +0100

    Created component for sliding images

--- src/app/(shop)/product-page/page.tsx
+++ src/app/(shop)/product-page/page.tsx
@@ -6,38 +6,14 @@ import { Layout, LayoutColumn, LayoutRow } from '@/components/ui/Layout';
 import { ArrowButton } from '@/components/ui/ArrowButton';
 import { SelectMaterial } from '@/components/shop/product-page/SelectMaterial';
 import { AddToCart } from '@/components/shop/product-page/AddToCart';
-
-// Images
-import ImagePalomaHeaven from '@/public/images/shop/paloma-heaven.png';
-import ImagePalomaHeavenDetails from '@/public/images/shop/paloma-heaven-details.png';
+import { ImageSlider } from '@/components/shop/product-page/ImageSlider';
 
 export default function ProductPage() {
   return (
     <Layout className="mt-32">
       <LayoutRow>
         <LayoutColumn xs={12} lg={7} className="pr-8">
-          <LayoutRow className="relative flex-nowrap overflow-x-scroll scroll-smooth lg:flex-row">
-            <LayoutColumn lg={8} className="flex-shrink-0 lg:pr-2">
-              <Image
-                src={ImagePalomaHeaven}
-                alt="Representaion of your wanted product"
-              />
-            </LayoutColumn>
-            <LayoutColumn lg={8} className="flex-shrink-0 lg:pl-2">
-              <Image
-                src={ImagePalomaHeavenDetails}
-                alt="Detailed representaion of your wanted product"
-              />
-            </LayoutColumn>
-            <div className="absolute left-4 top-1/2 flex w-[calc(100%-32px)] justify-between">
-              <ArrowButton variation="outline" />
-              <ArrowButton direction="right" />
-            </div>
-          </LayoutRow>
-          <div className="mt-6 flex justify-center text-md">
-            <p className="mr-4 underline underline-offset-4">1</p>
-            <p>2</p>
-          </div>
+          <ImageSlider />
         </LayoutColumn>
         <LayoutColumn xs={12} lg={5} className="flex flex-col pl-8">
           <p className="text-gray-500">Modern Luxe</p>
--- src/components/shop/product-page/ImageSlider.tsx
+++ src/components/shop/product-page/ImageSlider.tsx
@@ -1,5 +1,91 @@
-// External packages
+'use client';
+
+// Etxernal packages
+import * as React from 'react';
+import Image from 'next/image';
+
+// Components
+import { LayoutColumn, LayoutRow } from '@/components/ui/Layout';
+import { ArrowButton } from '@/components/ui/ArrowButton';
+
+// Images
+import ImagePalomaHeaven from '@/public/images/shop/paloma-heaven.png';
+import ImagePalomaHeavenDetails from '@/public/images/shop/paloma-heaven-details.png';
+import { twJoin } from 'tailwind-merge';
 
 export const ImageSlider = () => {
-  return <div>ImageSlider</div>;
+  const [imageCount, setImageCount] = React.useState(1);
+  const scrollableContainerRef = React.useRef<null | HTMLDivElement>(null);
+
+  // console.log(imageCount);
+  const scrollContainerFn = (isDirectionLeft: boolean = false) => {
+    const element = scrollableContainerRef.current;
+    if (element) {
+      if (isDirectionLeft) {
+        element.scrollLeft -= element.scrollWidth / 2;
+        setImageCount(imageCount - 1);
+      } else {
+        element.scrollLeft += element.scrollWidth / 2;
+        setImageCount(imageCount + 1);
+      }
+
+      console.log(imageCount);
+
+      if (imageCount === 2) {
+        element.scrollLeft -= 10000;
+        setImageCount(1);
+      }
+      if (imageCount === 1) {
+        element.scrollLeft = 1000;
+        setImageCount(2);
+      }
+    }
+  };
+  return (
+    <>
+      <div className="relative">
+        <LayoutRow
+          className="flex-nowrap overflow-x-scroll scroll-smooth lg:flex-row"
+          ref={scrollableContainerRef}
+        >
+          <LayoutColumn lg={8} className="flex-shrink-0 lg:pr-2">
+            <Image
+              src={ImagePalomaHeaven}
+              alt="Representaion of your wanted product"
+            />
+          </LayoutColumn>
+          <LayoutColumn lg={8} className="flex-shrink-0 lg:pl-2">
+            <Image
+              src={ImagePalomaHeavenDetails}
+              alt="Detailed representaion of your wanted product"
+            />
+          </LayoutColumn>
+        </LayoutRow>
+        <div className="absolute left-4 top-1/2 flex w-[calc(100%-32px)] justify-between">
+          <ArrowButton
+            variation="outline"
+            onPress={() => scrollContainerFn(true)}
+          />
+          <ArrowButton direction="right" onPress={() => scrollContainerFn()} />
+        </div>
+      </div>
+      <div className="mt-6 flex justify-center text-md">
+        <p
+          className={twJoin(
+            'mr-4',
+            imageCount === 1 && 'underline underline-offset-4'
+          )}
+        >
+          1
+        </p>
+        <p
+          className={
+            imageCount === 2 ? 'underline underline-offset-4' : undefined
+          }
+        >
+          2
+        </p>
+      </div>
+    </>
+  );
 };

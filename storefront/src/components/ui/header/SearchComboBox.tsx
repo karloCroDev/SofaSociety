4fcaf08a1700c4c00b5ec9cae57ccb1a0d439581
karloCroDev <karlo.webdev@gmail.com>
2025-07-25 16:41:21 +0200

    Search created, but cards are not pretty and needs to be decvoupled to react query

--- storefront/src/components/ui/header/SearchComboBox.tsx
+++ storefront/src/components/ui/header/SearchComboBox.tsx
@@ -9,28 +9,28 @@ import {
   Popover,
   ListBox,
 } from 'react-aria-components';
+import { HttpTypes } from '@medusajs/types';
+import { useRouter } from 'next/navigation';
 import { twJoin } from 'tailwind-merge';
-// import { Hit } from 'meilisearch';
-
-// Components
+//Components
 import { Icon } from '@/components/ui/Icon';
+import { CodeCountryTypes } from '@/components/ui/header/Header';
 
 // Lib
 import { MeiliSearchProductHit, searchClient } from '@/lib/search-client';
 import { getProductsById } from '@/lib/data/products';
-import { getProductPrice } from '@/lib/util/get-product-price';
-import { CodeCountryTypes } from '@/components/ui/header/Header';
+
+// Hooks
 import { useCountryCode } from '@/hooks/country-code';
 import { useDebounce } from '@/hooks2/useDebounce';
-import { HttpTypes } from '@medusajs/types';
-import Image from 'next/image';
-import Link from 'next/link';
 
-export const SearchComboBox: React.FC<{
-  codeCountry: CodeCountryTypes;
-}> = ({ codeCountry }) => {
+export const SearchComboBox: React.FC<{ codeCountry: CodeCountryTypes }> = ({
+  codeCountry,
+}) => {
+  const router = useRouter();
   const countryCode = useCountryCode();
   const region = codeCountry.find((x) => x.country === countryCode)?.id;
+
   const [displayComboBox, setDisplayComboBox] = React.useState(false);
   const [value, setValue] = React.useState('');
 
@@ -39,12 +39,12 @@ export const SearchComboBox: React.FC<{
 
   const debounceValue = useDebounce(value);
 
-  // Move this to work with react query!
   React.useEffect(() => {
     const findQueries = async () => {
       const results = await searchClient
         .index('products')
         .search<MeiliSearchProductHit>(debounceValue, undefined);
+
       const medusaProducts = await getProductsById({
         ids: results.hits.map((h) => h.id),
         regionId: region!,
@@ -52,12 +52,10 @@ export const SearchComboBox: React.FC<{
 
       setProducts(medusaProducts);
     };
-    hasLoaded && findQueries(); // Making sure that this will be only displayed once a text changes, not immediately
+    hasLoaded && debounceValue && findQueries();
 
     setHasLoaded(true);
   }, [debounceValue]);
-
-  console.log(products);
 
   return (
     <div className="flex items-center gap-4">
@@ -73,25 +71,23 @@ export const SearchComboBox: React.FC<{
           displayComboBox ? 'w-36' : 'w-0'
         )}
       >
-        <ComboBox>
-          <Input
-            className="w-full border-0 border-b border-b-current bg-inherit outline-none"
-            onChange={(e) => setValue(e.target.value)}
-          />
+        <ComboBox inputValue={value} onInputChange={setValue} items={products}>
+          <Input className="w-full border-0 border-b border-b-current bg-inherit outline-none" />
           <Popover className="w-36">
             <ListBox className="flex flex-col gap-y-4 rounded border border-gray-900 bg-gray-10 p-2">
-              {products.map((product) => (
-                <ListBoxItem key={product.id}>
-                  <Link href={`/product/${product.handle}`}>
-                    <Image
-                      src={product.thumbnail || ''}
-                      alt={product.description || ''}
-                    />
-                    <p>{product.title}</p>
-                  </Link>
-                </ListBoxItem>
-              ))}
-              <ListBoxItem>Aardvark</ListBoxItem>
+              {products.length > 0 ? (
+                products.map((product) => (
+                  <ListBoxItem
+                    key={product.id}
+                    onAction={() => router.push(`/product/${product.handle}`)}
+                    className="cursor-pointer"
+                  >
+                    {product.title}
+                  </ListBoxItem>
+                ))
+              ) : (
+                <ListBoxItem>No products found</ListBoxItem>
+              )}
             </ListBox>
           </Popover>
         </ComboBox>
